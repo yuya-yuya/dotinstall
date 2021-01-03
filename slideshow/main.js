ca6487c8a27fac444a65509ebbb94b89ad146d3d
yuya-yuya <yytyfp5115@icloud.com>
2021-01-03 16:01:55 +0900

    addtional commit

--- slideshow/main.js
+++ slideshow/main.js
@@ -15,4 +15,33 @@
 
   const mainImage = document.getElementById('main');
   mainImage.src = images[currentIndex];
+
+  images.forEach(function(image , index){
+    const img = document.createElement('img');
+    img.src = image;
+
+    const li = document.createElement('li');
+    if (index === currentIndex) {
+      li.classList.add('current');
+    }
+    li.addEventListener('click', () => {
+      mainImage.src = image;
+      const thumbnails = document.querySelectorAll('.thumbnails > li');
+      thumbnails[currentIndex].classList.remove('current');
+      currentIndex = index;
+      thumbnails[currentIndex].classList.add('current');
+    });
+
+    li.appendChild(img);
+    document.querySelector('.thumbnails').appendChild(li);
+  });
+
+  const next = document.getElementById('next');
+  next.addEventListener('click', () => {
+    let target = currentIndex + 1;
+    if (target === images.length) {
+      target = 0;
+    }
+    document.querySelectorAll('.thumbnails > li')[target].click();
+  });
 }
